4e3b4cdbafd3f56f1ce06b656b2561087d0de0be
Peter Levinsky <pele@easj.dk>
2021-09-02 12:53:10 +0200

    Add project files.

---
+++ ClassDemo3CEchoApp/EchoWorker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace ClassDemo3CEchoApp
+{
+    internal class EchoWorker
+    {
+        public EchoWorker()
+        {
+        }
+
+        public void Start()
+        {
+            TcpListener listener = new TcpListener(7777);
+            listener.Start();
+
+            TcpClient socket = listener.AcceptTcpClient();
+
+            using(StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+            {
+                String str = sr.ReadLine();
+                Console.WriteLine("Server har modtaget : " + str);
+
+                sw.WriteLine(str);
+                sw.Flush();
+            }
+
+        }
+    }
+}
---
+++ ClassDemo3CEchoApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassDemo3CEchoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EchoWorker worker = new EchoWorker();
+            worker.Start();
+
+
+        }
+    }
+}
